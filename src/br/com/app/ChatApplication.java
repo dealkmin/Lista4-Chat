f464ef5dac2e6de900b5c295be3e551ce8f8aa47
Deborah Alkmin <deborah_alkmin@hotmail.com>
2020-06-18 22:10:35 -0300

    Cria server

--- src/br/com/app/ChatApplication.java
+++ src/br/com/app/ChatApplication.java
@@ -1,9 +1,42 @@
 package br.com.app;
 
+import javax.swing.JOptionPane;
+
+import br.com.server.Server;
+
 public class ChatApplication {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String addr;
+		if(args.length > 0) {
+			addr = args[1];
+		} else {
+			addr = "172.18.0.1";
+		}
+		// conexao socket tcp com servidor
+		Client conn = null;
+		try {
+			conn = new Client(addr, Server.PORT);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// nome do cliente
+		String name = "";
+		while((name == null)|| name.length() == 0) { // le o nome
+			name = JOptionPane.showInputDialog("Nome:");
+		}
+		name = name.trim(); // limpa espaços do começo e fim
+		try {
+			conn.send(name, false);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} // manda para o servidor sem criptografar
+        // abre a janela do chat
+		final ChatWindow cw = ChatWindow.open(conn, name);
+		// thread para ouvir mensagens
+		cw.listener.start();
 
 	}
 
--- src/br/com/server/HandleClient.java
+++ src/br/com/server/HandleClient.java
@@ -1,5 +1,51 @@
 package br.com.server;
 
 public class HandleClient {
+	
+	final private static String WARNING = "\\warning";
+	private ClientInfo client;
+	private Server server;
+	
+	public HandleClient(ClientInfo client, Server server) {
+		this.client = client;
+		this.server = server;
+	}
+	
+	public void run() {
+
+		// Envia uma mensagem ao cliente solicitando seu nome
+		server.readClientName(client);
+		
+		// Informa a entrada do cliente à todos os outros conectados
+		server.broadcast(null, WARNING+ client.name + " entrou!");
+		
+		// adiciona o cliente na lista de clients conectados
+		server.clients.add(client);
+		
+		String msg;
+		while(true) {
+			try {
+				msg = client.reader.readLine(); // lê uma mensagem enviada pelo clientes
+				if(msg == null) {
+					break;
+				}
+				System.out.println(msg); // log das mensagens enviadas
+				server.broadcast(client, msg); // replica a mensagem enviada
+			}
+			catch(Exception e) {
+				break;
+			}
+		}
+		
+		// log de saída
+		System.out.println("Cliente " + client.conn.getRemoteSocketAddress() + " saiu!");
+		
+		// disconecta o cliente do servidor
+		client.disconnect();
+
+		// Informa a entrada do cliente à todos os outros conectados
+		server.broadcast(null, WARNING+client.name + " saiu!");
+		
+	}
 
 }
