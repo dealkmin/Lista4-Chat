a08c0c1491684cfc95a01af3386d4f8a9aee0b32
Vitor Lescura <vitorlescura@gmail.com>
2020-06-18 21:57:06 -0300

    Cria Client

--- src/br/com/app/Client.java
+++ src/br/com/app/Client.java
@@ -1,5 +1,74 @@
 package br.com.app;
 
-public class Client {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
+public class Client {
+	private final static int SECRET = 13;
+	private Socket socket;
+	private InputStream in;
+	private InputStreamReader inr;
+	private BufferedReader bfr;
+	private PrintWriter pw;
+
+	public Client(String ip, int door) throws Exception {
+		this.socket = new Socket(ip, door);
+		this.in = socket.getInputStream();
+		this.inr = new InputStreamReader(in, StandardCharsets.UTF_8);
+		this.bfr = new BufferedReader(inr);
+		this.pw = new PrintWriter(
+				new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8), true
+		);
+	}
+	
+	public void send(String msg)throws Exception {
+		send(msg, true);
+	}
+	
+	public void send(String msg, boolean hidden) throws Exception {
+		if(hidden) {
+			msg = encrypt(msg);
+		}
+		pw.println(msg);
+	}
+
+	private static String encrypt(String msg) {
+		return secret(msg, SECRET);
+	}
+	
+	private static String decrypt(String msg) {
+		return secret(msg, -SECRET);
+	}
+	
+	private static String secret(String msg, int method) {
+		char[] msgEncoded = new char[msg.length()];
+		int i = 0;
+		for(char ch : msg.toCharArray()) {
+			msgEncoded[i++] = (char) ((int) ch + method); 
+		}
+		String s = new String(msgEncoded);
+		return s;
+	}
+
+	public String listen() throws IOException{
+		String msg = null;                        
+		if(bfr.ready()){
+			msg = bfr.readLine();
+			if(msg.startsWith("\\warning")) {
+				return msg.substring(8);
+			}
+			return decrypt(msg);
+		}
+		return msg;
+	}
+	
+	public void close() throws Exception {
+		socket.close();
+	}
 }
