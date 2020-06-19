55df0ef0a1e3544be978607563d3c2682afb049e
Deborah Alkmin <deborah_alkmin@hotmail.com>
2020-06-18 21:31:08 -0300

    Cria classe controle

--- src/br/com/negocio/Controle.java
+++ src/br/com/negocio/Controle.java
@@ -1,5 +1,33 @@
 package br.com.negocio;
 
+import java.util.Scanner;
+
 public class Controle {
+	
+	Scanner in;
+	
+	public Controle(){
+		in = new Scanner(System.in);
+	}
+	
+	public int readInt() {
+		if( in.hasNextInt() ) {
+			return in.nextInt();
+		}
+		in.nextLine();
+		return -1;
+	}
+	
+	public String readLine() {
+		if ( in.hasNextLine() ) {
+			in.nextLine();
+		}
+		return in.nextLine().trim();
+	}
+	public void await() {
+		System.out.println("...");
+		this.readLine();
+		
+	}
 
 }
